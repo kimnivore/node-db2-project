ebc4eb41a5b993d5e064f83903cdb20338fcc4b3
Kim Nguyen <kimnivore@gmail.com>
2022-02-15 11:28:30 -0800

    created models

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -4,12 +4,12 @@ exports.up = function (knex) {
     tbl.string('vin', 128).unique().notNullable();
     tbl.string('make', 128).notNullable();
     tbl.string('model', 128).notNullable();
-    tbl.numeric('mileage').notNullable();
+    tbl.decimal('mileage').notNullable();
     tbl.string('title', 128);
     tbl.string('transmission', 128);
   })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
